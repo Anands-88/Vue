03fd8e733f8137372395a2a5fd1e17724cf2befd
Anands-88 <anandsholla8@gmail.com>
2022-09-06 22:22:19 +0530

    Add vue codes

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,16 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>{{name}}</h1>
+    <h2 v-text="language"></h2>
+    <h3 v-html="content"></h3>
+    <h4>{{bindit}}</h4>
+    <h5 class="movies" v-bind:class='isOld ? "old" : "New"'>Movies</h5>
+    <button v-bind:id="bindit" disabled="disabled">button</button>
+  </div>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name:"Anand",
+      language:"Vue",
+      content:`<kbd>Hello world</kbd>`,
+      bindit:"Button",
+      disabled:false,
+      isOld:false,
+      isNew:true
+    }
   }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
@@ -22,5 +40,29 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.movies{
+  font-size:20px;
+  width:80px;
+  height:30px;
+}
+
+.old{
+  background:black;
+  color:white;
+  
+}
+
+.New{
+  background:orange;
+  color:blue;
+}
+
+
+#Button{
+  background:red;
+  color:white;
+  font-size:38px;
 }
 </style>
