9acf3b08111d72d400ba80a32af08d3c002f52bf
Anands-88 <anandsholla8@gmail.com>
2022-10-25 13:08:31 +0530

    Directives updates

--- Vue/src/components/Udemy/udemy_vue.vue
+++ Vue/src/components/Udemy/udemy_vue.vue
@@ -2,19 +2,26 @@
   <div>
     <!-- <input type="text" v-on:input="getValue($event)"> -->
     <h3>Click here to Learn Vue <a v-bind:href="vueLink">Vue</a> </h3>
-    <input type="text" v-model="word">
+    <input type="text" v-model="word" v-on:keyup.enter="confirmName"> <!-- displayName only on enter key-->
     <button @click="displayValue">Click</button>
+    <h2>{{finalName}}</h2>
     <ol>
       <li v-for="(item,index) in array" :key="index">{{item}}</li>
     </ol>
     <p v-html="htmlEle"></p>
+    <kbd v-once style="font-size:24px;">Initial Count : {{counter}}</kbd> <!--Update the value only once-->
+    <br>
     <kbd style="font-size:24px;">Total Count : {{counter}}</kbd>
     <div class="buttonBox">
       <!-- <button v-on:click="counter++">Add</button> -->
-      <button v-on:click="changeCount(2)">Add 2</button>
-      <button v-on:click="changeCount(-1)">Remove</button>
+      <button v-on:click.right="changeCount(2)">Add 2</button> <!--Only right click-->
+      <button v-on:click.left="changeCount(-1)">Remove</button> <!--Only lerightft click-->
       <!-- <button v-on:click="counter = counter - 2">Remove 2</button> -->
     </div>
+      <form action="" v-on:submit.prevent="submitForm">
+        <input type="text" placeholder="Form submission test">
+        <button>Submit</button>
+      </form>
   </div>
 
 </template>
@@ -28,8 +35,9 @@ export default {
       array: [],
       word: "",
       vueLink: "https://vuejs.org",
-      counter: 0,
+      counter: 4,
       htmlEle: `<h4>Learn Vue</h4>`,
+      finalName:""
     }
   },
 
@@ -37,6 +45,13 @@ export default {
     // getValue(e){
     //   this.word = e.target.value
     // },
+    confirmName(){
+      this.finalName = this.word
+    },
+    submitForm(){ //event
+      // event.preventDefault()
+      alert('Hello')
+    },
     displayValue() {
       this.array.push(`Total Count is ${this.counter}`)
       this.word = ""
